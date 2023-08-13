00b14b1e735d18a7f95bfde8991f22fdd5069c8b
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-08-13 15:07:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@
 
 module.exports = function towelSort (matrix) {
   for (let i = 0; i < matrix.length; i++) {
-    if (i == 1) {
-      matrix[i].sort((a, b) => b - a);
-    } else matrix[i].sort((a, b) => a - b);
+    if (i == 0) {
+      matrix[i].sort((a, b) => a - b);
+    }
+    console.log(i, i % 2 === 0);
+    if (i % 2 === 0) {
+      matrix[i].sort((a, b) => a - b);
+    } else matrix[i].sort((a, b) => b - a);
   }
   return matrix.flat();
 }
